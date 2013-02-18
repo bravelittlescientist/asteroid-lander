d02f06594a7bfeae7b813bea1738f6071e271b7c
saini <vaibps17@gmail.com>
2013-02-17 22:17:00 -0800

    buy fuel, assign plot, and some changes to updateSelfScore

--- game/BaseStationModel.py
+++ game/BaseStationModel.py
@@ -3,20 +3,32 @@ Created on 16/02/2013
 
 @author: Christian Adriano
 '''
-
+from Constants import *
+from game import GameRule
 class BaseStationModel(object):
     '''
     This class holds the data shared between the client (Spaceship) and the GameServer.
     This class is used solely by the Spaceship.
     '''
-    fuel = 0
-    gameScore = [0,0,0]
-    mineGrid = [0,0,0]
+    
 
     def __init__(self, selfparams):
         '''
         Constructor
         '''
+        self.fuel = MAX_FUEL_BASE_STATION
+        self.gameScore = {GOLD:0,
+                          COPPER:0,
+                          IRON:0}
+        self.mineGrid = {GOLD:GOLD_PLOT_TOTAL,
+                          COPPER:COPPER_PLOT_TOTAL,
+                          IRON:IRON_PLOT_TOTAL}
+        self.conquredPlot = {GOLD:0,
+                          COPPER:0,
+                          IRON:0}
+        
+        
+        self.gameRule = GameRule()
         
     def setgameScore(self, i,value):
         self.gameScore[i] = value
@@ -26,3 +38,29 @@ class BaseStationModel(object):
         
     def setfuel(self, value):
         self.fuel= value
+        
+    def buyFuel(self):
+        self.gameScore[GOLD] = self.gameScore[GOLD]-self.gameRule.buyRate
+        self.fuel = min(MAX_FUEL_BASE_STATION, self.fuel+self.gameRule.fuelPerExchangeUnit)
+    
+    def canBuyFuel(self):
+        if self.fuel == MAX_FUEL_BASE_STATION:
+            return (False, "BASE STATION FUEL TANK ALREADY AT MAX CAPACITY" )
+        if self.gameScore[GOLD]>= self.gameRule.buyRate:
+            return (False, "NOT ENOUGH GOLD TO BUY FUEL")
+        return ((self.fuel < MAX_FUEL_BASE_STATION) and (self.gameScore[GOLD]>= self.gameRule.buyRate),"SUCCESS")
+    
+    def assignPlot(self,type):
+        self.mineGrid[type] = self.mineGrid[type]-1
+    
+    def canAssignPlot(self,type):
+        if self.mineGrid[type]<1:
+            return (False, "NO MORE PLOTS AVAILABLE OF THIS TYPE")
+        return (True,"SUCCESS")
+    
+    def conquerPlot(self,type):
+        if self.conqueredPlot[type]< self.gameRule.maxPlotCount[type]:
+            self.conquredPlot[type] = self.conquredPlot[type]+1
+            #TODO: add other things here like what happens when a plot gets conqured.
+    
+        
