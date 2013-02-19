22e4d01030eefc0ca0fd38b481be4cef07b5ecb8
saini <vaibps17@gmail.com>
2013-02-18 21:41:39 -0800

    Adding refactored files

---
+++ game/model/GameRule.py
@@ -0,0 +1,31 @@
+'''
+Created on Feb 17, 2013
+
+@author: vaibhavsaini
+'''
+from game.Constants import *
+class GameRule(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        self.buyRate = BUY_RATE
+        self.exchangeUnit = GOLD
+        self.fuelPerExchangeUnit = FUEL_EXCHANGE
+        self.gameGoal = {GOLD:30,
+                          COPPER:80,
+                          IRON:120}
+        self.maxPlotCount = {GOLD:GOLD_PLOT_TOTAL,
+                          COPPER:COPPER_PLOT_TOTAL,
+                          IRON:IRON_PLOT_TOTAL}
+    
+    def getBuyRate(self):
+        return self.buyRate
+    
+    def getExchangeUnit(self):
+        return self.exchangeUnit
---
+++ game/model/SpaceshipModel.py
@@ -0,0 +1,42 @@
+'''
+Created on 16/02/2013
+
+@author: Christian Adriano
+'''
+from game.Constants import *
+
+
+class SpaceshipModel(object):
+    '''
+    Keeps all the state variables of the Spaceship
+    '''
+
+    altitude = 1000
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.active= True
+        self._server.addPlayer() # update the number of active players
+        self.score = 0;
+        self.assignedPlot = 0
+        self.mass = SPACESHIP_OWN_MASS
+        self.cargoCapacity = SPACESHIP_CARGO_CAPACITY
+        self.minerals={GOLD:0,
+                       COPPER:0,
+                       IRON:0}
+        
+    def setAltitude(self, value):
+        self.altitude = value
+        
+    def getAltitude(self): 
+        return self.altitude
+    
+    def GetAvailableCapacity(self):
+        used = 0
+        for key in self.minerals.keys():
+            used +=self.minerals[key]
+        return self.cargoCapacity-used
+    
+    
